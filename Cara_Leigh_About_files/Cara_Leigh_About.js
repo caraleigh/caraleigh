787ac8e997d47945806e417e305987608dfcc938
Cara Leigh <caraleighmusic@gmail.com>
2012-08-05 20:23:08 -0400

    testing out iweb

---
+++ Cara_Leigh_About_files/Cara_Leigh_About.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0 local-build-20120805
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000}),stroke_0:new IWStrokeParts([{rect:new IWRect(-2,2,4,117),url:'Cara_Leigh_About_files/stroke.png'},{rect:new IWRect(-2,-2,4,4),url:'Cara_Leigh_About_files/stroke_1.png'},{rect:new IWRect(2,-2,106,4),url:'Cara_Leigh_About_files/stroke_2.png'},{rect:new IWRect(108,-2,4,4),url:'Cara_Leigh_About_files/stroke_3.png'},{rect:new IWRect(108,2,4,117),url:'Cara_Leigh_About_files/stroke_4.png'},{rect:new IWRect(108,119,4,5),url:'Cara_Leigh_About_files/stroke_5.png'},{rect:new IWRect(2,119,106,5),url:'Cara_Leigh_About_files/stroke_6.png'},{rect:new IWRect(-2,119,4,5),url:'Cara_Leigh_About_files/stroke_7.png'}],new IWSize(110,122)),shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(4.2426,4.2426),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Cara_Leigh_About_files/Cara_Leigh_AboutMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');detectBrowser();adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');adjustLineHeightIfTooBig('id11');adjustFontSizeIfTooBig('id11');fixAllIEPNGs('Media/transparent.gif');IMpreload('Cara_Leigh_About_files','shapeimage_3','0');IMpreload('Cara_Leigh_About_files','shapeimage_4','0');IMpreload('Cara_Leigh_About_files','shapeimage_5','0');Widget.onload();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
