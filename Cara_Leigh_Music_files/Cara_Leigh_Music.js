118e202093b76555cf231726182c5215f95f44a1
Cara Leigh <caraleighmusic@gmail.com>
2012-08-08 21:30:25 -0400

    social media toolbar update

--- Cara_Leigh_Music_files/Cara_Leigh_Music.js
+++ Cara_Leigh_Music_files/Cara_Leigh_Music.js
@@ -1,11 +1,11 @@
 // Created by iWeb 3.0 local-build-20120808
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:6,offset:new IWPoint(-0.8998,3.8975),color:'#000000',opacity:0.750000}),shadow_1:new IWShadow({blurRadius:6,offset:new IWPoint(0.4875,3.9702),color:'#000000',opacity:0.750000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_1:new IWShadow({blurRadius:6,offset:new IWPoint(0.4875,3.9702),color:'#000000',opacity:0.750000}),shadow_0:new IWShadow({blurRadius:6,offset:new IWPoint(-0.8998,3.8975),color:'#000000',opacity:0.750000})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Cara_Leigh_Music_files/Cara_Leigh_MusicMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');detectBrowser();adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');fixAllIEPNGs('Media/transparent.gif');IMpreload('Cara_Leigh_Music_files','shapeimage_3','0');IMpreload('Cara_Leigh_Music_files','shapeimage_4','0');IMpreload('Cara_Leigh_Music_files','shapeimage_5','0');Widget.onload();fixupAllIEPNGBGs();applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');detectBrowser();adjustLineHeightIfTooBig('id10');adjustFontSizeIfTooBig('id10');fixAllIEPNGs('Media/transparent.gif');IMpreload('Cara_Leigh_Music_files','shapeimage_3','0');IMpreload('Cara_Leigh_Music_files','shapeimage_4','0');IMpreload('Cara_Leigh_Music_files','shapeimage_5','0');Widget.onload();applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
