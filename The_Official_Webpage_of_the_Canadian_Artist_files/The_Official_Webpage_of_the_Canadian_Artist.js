e4eabc4d3cd3f606426456d5b756f17173af5de5
Cara Leigh <caraleighmusic@gmail.com>
2013-03-25 07:48:19 -0400

    new website - trying it out

---
+++ The_Official_Webpage_of_the_Canadian_Artist_files/The_Official_Webpage_of_the_Canadian_Artist.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0 local-build-20130325
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('The_Official_Webpage_of_the_Canadian_Artist_files/The_Official_Webpage_of_the_Canadian_ArtistMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
