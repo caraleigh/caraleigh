d8dd2a5ec5a1e45ed020e9983b4ab7427615d6bf
Cara Leigh <caraleighmusic@gmail.com>
2013-05-06 00:04:11 -0400

    trying out new website layout

---
+++ caraleigh/Cara_Leigh_files/Cara_Leigh.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0 local-build-20130506
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_1:new IWShadow({blurRadius:1,offset:new IWPoint(-2.6239,1.4544),color:'#000000',opacity:0.700000}),shadow_3:new IWShadow({blurRadius:1,offset:new IWPoint(-2.6239,1.4544),color:'#000000',opacity:0.700000}),shadow_5:new IWShadow({blurRadius:1,offset:new IWPoint(-2.6239,1.4544),color:'#000000',opacity:0.700000}),shadow_4:new IWShadow({blurRadius:1,offset:new IWPoint(-2.6239,1.4544),color:'#000000',opacity:0.700000}),shadow_2:new IWShadow({blurRadius:1,offset:new IWPoint(-2.6239,1.4544),color:'#000000',opacity:0.700000}),shadow_0:new IWShadow({blurRadius:1,offset:new IWPoint(-2.6239,1.4544),color:'#000000',opacity:0.700000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Cara_Leigh_files/Cara_LeighMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();fixupIECSS3Opacity('id8');fixupIECSS3Opacity('id9');fixupIECSS3Opacity('id10');fixupIECSS3Opacity('id11');fixupIECSS3Opacity('id12');fixupIECSS3Opacity('id13');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
